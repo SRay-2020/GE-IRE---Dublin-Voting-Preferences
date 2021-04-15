d4145c0bc2770905985c1bae03601edead6da155
SRay-2020 <stephen.r.ray@gmail.com>
2021-04-15 11:53:36 +0000

    Add second page js and styles

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var li_elements = document.querySelectorAll(".wrapper-left ul li");
-
+var item_elements = document.querySelectorAll(".item");
 for (var i = 0; i < li_elements.length; i++) {
     li_elements[i].addEventListener("click", function () {
         li_elements.forEach(function (li) {
@@ -7,5 +7,22 @@ for (var i = 0; i < li_elements.length; i++) {
         })
         this.classList.add("active");
         var li_value = this.getAttribute("data-li");
-    })
+
+        item_elements.forEach(function (item) {
+            item.style.display = "none";
+        })
+
+
+        if (li_value == "fingal") {
+            document.querySelector("." + li_value).style.display = "block";
+        } else if (li_value == "dbn") {
+            document.querySelector("." + li_value).style.display = "block";
+        } else if (li_value == "dbs") {
+            document.querySelector("." + li_value).style.display = "block";
+        } else if (li_value == "dc") {
+            document.querySelector("." + li_value).style.display = "block";
+        } else {
+            console.log("");
+        }
+    });
 }
