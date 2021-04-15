dc4cf89504a060987d969d0eded8492649dabd47
SRay-2020 <stephen.r.ray@gmail.com>
2021-04-15 17:42:53 +0000

    Unfix map links

--- direct_map.js
+++ direct_map.js
@@ -1,8 +1,20 @@
-if addEventListener click = true on dubbn
+// if addEventListener click = true on dubbn
 
-let {
-    link to page 2 &&
-        li fingal = active &&
-        item fingal = block display true
+// let {
+//     link to page 2 &&
+//         li fingal = active &&
+//         item fingal = block display true
 
-}
+// }
+// var map_elements = document.querySelectorAll(".cvg a");
+
+
+// document.getElementById("dubBayer").addEventListener("click", myMapPath);
+
+// function myMapPath() {
+//     location.href = "https://www.google.ie"
+// }
+
+document.getElementById("dubBayer").onclick = function () {
+    location.href = "www.google.ie";
+};
