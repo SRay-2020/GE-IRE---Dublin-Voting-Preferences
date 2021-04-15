19021b6477aaccb6e5a42a1222f4d2dcd8ec2dcd
SRay-2020 <stephen.r.ray@gmail.com>
2021-04-15 15:45:26 +0000

    Fix link start

---
+++ direct_map.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+
+    $(".dubwest").click(function () {
+        $(".navLink").removeClass("active");
+        $(this).addClass("active");
+    });
+});
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var li_elements = document.querySelectorAll(".wrapper-left ul li");
 var item_elements = document.querySelectorAll(".item");
+var map_elements = document.querySelectorAll(".svg a")
 for (var i = 0; i < li_elements.length; i++) {
     li_elements[i].addEventListener("click", function () {
         li_elements.forEach(function (li) {
@@ -8,9 +9,16 @@ for (var i = 0; i < li_elements.length; i++) {
         this.classList.add("active");
         var li_value = this.getAttribute("data-li");
 
+        map_elements.forEach(function (a) {
+            a.classList.remove("active");
+        })
+        this.classList.add("active");
+        var li_value = this.getAttribute("data-li");
+
         item_elements.forEach(function (item) {
             item.style.display = "none";
         })
+
 
         if (li_value == "fingal") {
             document.querySelector("." + li_value).style.display = "block";
