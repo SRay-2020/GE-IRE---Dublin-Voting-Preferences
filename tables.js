2e0f3dd93ebb00c05267ec1efebee5fc3a91cd20
SRay-2020 <stephen.r.ray@gmail.com>
2021-04-23 15:36:19 +0000

    Add data to main table pg 1

--- tables.js
+++ tables.js
@@ -2,18 +2,19 @@
   var myChart = new Chart(ctx, {
       type: 'bar',
       data: {
-          labels: ['SF', 'FG', 'FF', 'LAB', 'SD', 'GP', 'S-PBP', 'IND'],
+          labels: ['SF', 'FG', 'FF', 'GP', 'S-PBP', 'LAB', 'SD', 'I4C', 'IND/OTH'],
           datasets: [{
               label: 'Percent of First Preference Votes',
-              data: [29.81, 18.76, 14.38, 11.35, 8.70, 7.04, 2.98, 5.61],
+              data: [28.41, 19.73, 14.42, 11.80, 6.24, 5.91, 5.16, 1.02, 7.35],
               backgroundColor: [
                   'rgba(09, 53, 05, 1)',
                   'rgba(06, 02, 197, 1)',
                   'rgba(26, 151, 22, 1)',
-                  'rgba(255, 0, 0, 1)',
-                  'rgba(97, 1, 187, 1)',
                   'rgba(83, 151, 77, 1)',
                   'rgba(110, 46, 9, 1)',
+                  'rgba(255, 0, 0, 1)',
+                  'rgba(97, 1, 187, 1)',
+                  'rgba(209, 95, 212, 1)',
                   'rgba(128, 128, 128, 1)'
               ],
               borderColor: [
